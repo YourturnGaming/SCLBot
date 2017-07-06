3795f02bcfebf58d5c1c5fdb917903ff448a3f2e
TitanMusic | Developer <estmercyclan@gmail.com>
2017-07-06 03:18:59 +0300

    2.11.7

--- TMBot.js
+++ TMBot.js
@@ -3049,10 +3049,10 @@
                 rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("Titan Music public discord: https://discord.gg/hvWTZrZ");
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!bot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                            API.sendChat("/me Titan Music public discord: https://discord.gg/hvWTZrZ");
                             }));
                         }
                     }
