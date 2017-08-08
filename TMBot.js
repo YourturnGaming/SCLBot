36bdb7b51ae57c52c293d7e17578ee2a74cef28b
MsiGaming | Developer <estmercyclan@gmail.com>
2017-08-08 22:30:35 +0300

    Update TMBot js

--- TMBot.js
+++ TMBot.js
@@ -3049,7 +3049,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!bot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                            API.sendChat("/me Titan Music public discord: Update https://discord.gg/msKJ5u");
+                            API.sendChat("/me Titan Music public discord: https://discord.gg/msKJ5u");
                     }
                 }
             },
