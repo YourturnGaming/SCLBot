d5c6cda093770d0fb74456c0408de2d343daacc4
MsiGaming | Developer <estmercyclan@gmail.com>
2017-10-14 00:39:21 +0300

    New Update

--- TMBot.js
+++ TMBot.js
@@ -4292,7 +4292,20 @@
                 }
             }
         }
-    };
+    },
+     
+    guideCommand: {
+       command: 'guide',
+       rank: 'user',
+       type: 'exact',
+       functionality: function(chat, cmd) {
+           if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+           if (!bot.commands.executable(this.rank, chat)) return void (0);
+           else {
+                   API.sendChat("plug.dj guide: http://i.imgur.com/ZeRR07N.png");
+          )
+       )
+    ),   
 
     loadChat(TMBot.startup);
 }).call(this);
